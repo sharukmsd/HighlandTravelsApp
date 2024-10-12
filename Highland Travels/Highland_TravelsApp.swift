e75e037ba804b56f343c92583730130657ca0410
Shahrukh <sharuk.msd6@gmail.com>
2024-10-12 13:13:08 +0500

    Initial Commit

---
+++ Highland Travels/Highland_TravelsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Highland_TravelsApp.swift
+//  Highland Travels
+//
+//  Created by Shahrukh on 12/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct Highland_TravelsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
